56dd00d80c4a39416aa79ff23fd3156941c6512f
kure-t <tatuya.kure@gmail.com>
2026-03-21 16:48:25 +0900

    cache

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -11,6 +11,7 @@ import {
 export const staticRouter = Router();
 
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
+staticRouter.use(history());
 
 staticRouter.use(
   serveStatic(UPLOAD_PATH, {
@@ -37,41 +38,19 @@ staticRouter.use(
   serveStatic(CLIENT_DIST_PATH, {
     etag: true,
     lastModified: false,
-    // maxAge: 31536000,
-    // immutable: true,
-    // setHeaders: (res, path) => {
-    //   if (path.endsWith(".html")) {
-    //     // HTMLは再検証
-    //     res.setHeader("Cache-Control", "no-cache");
-    //   } else {
-    //     // その他は強キャッシュ
-    //     res.setHeader(
-    //       "Cache-Control",
-    //       "public, max-age=31536000, immutable"
-    //     );
-    //   }
-    // },
-  }),
-);
-
-staticRouter.use(history());
-staticRouter.use(
-  serveStatic(CLIENT_DIST_PATH, {
-    etag: true,
-    lastModified: false,
-    // maxAge: 31536000,
-    // immutable: true,
-    // setHeaders: (res, path) => {
-    //   if (path.endsWith(".html")) {
-    //     // HTMLは再検証
-    //     res.setHeader("Cache-Control", "no-cache");
-    //   } else {
-    //     // その他は強キャッシュ
-    //     res.setHeader(
-    //       "Cache-Control",
-    //       "public, max-age=31536000, immutable"
-    //     );
-    //   }
-    // },
+    maxAge: 31536000,
+    immutable: true,
+    setHeaders: (res, path) => {
+      if (path.endsWith(".html")) {
+        // HTMLは再検証
+        res.setHeader("Cache-Control", "no-cache");
+      } else {
+        // その他は強キャッシュ
+        res.setHeader(
+          "Cache-Control",
+          "public, max-age=31536000, immutable"
+        );
+      }
+    },
   }),
 );
